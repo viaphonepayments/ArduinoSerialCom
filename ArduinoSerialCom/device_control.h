777678a621b06391f651ead9c02d1a1e50b4aa56
ankol <ankol@ANCALLED-WIN10>
2016-07-22 11:32:58 +0600

    Add project files.

---
+++ ArduinoSerialCom/device_control.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "tserial.h"
+#include <string>
+#include <tchar.h>
+
+#define CHIRP_LEN 18
+
+class Device {
+
+private:
+	
+	Tserial *com;
+
+public:
+
+	Device() {
+	}
+
+	bool startDevice(char *port, int speed)	{
+		com = new Tserial();
+		if (com != 0) {
+			if (com->connect(port, speed, spNONE)) {
+				printf("Not Connected...\n");
+			} 
+			else {
+				printf("Connected..\n");
+			}
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
+	}
+
+	void stopDevice() {
+		com->disconnect();
+		// ------------------
+		delete com;
+		com = 0;
+	}
+
+	void send_data(unsigned char data) {
+		com->sendChar(data);
+		printf("%c", data);
+	}
+
+	void device_name() {
+		char *cmd = "ua;";
+		com->sendArray(cmd, strlen(cmd));
+
+		char* buffer = new char[1024];
+		com->getArray(buffer, 1024);
+
+
+		printf("%c", buffer);
+
+		delete buffer;
+	}
+
+	int playChirp(const char* chirp) {
+		int len = strlen(chirp);
+		if (len != CHIRP_LEN) {
+			return -1;
+		}
+		char cmd[CHIRP_LEN + 4];
+		cmd[0] = '\0';
+		strcat(cmd, "ch");
+		strcat(cmd, chirp);
+		strcat(cmd, ";");
+		com->sendArray(cmd, strlen(cmd));
+		return 0;
+	}
+
+};
---
+++ ArduinoSerialCom/main.cpp
@@ -0,0 +1,28 @@
+// ArduinoSerialCom.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include "tserial.h"
+#include "device_control.h"
+
+Device device;
+
+int main()
+{
+	device.startDevice("COM3", 9600);
+	
+	//printf("Retreiving device name...");
+
+	//device.device_name();
+	//device.device_name();
+
+	printf("Sending chirp...");
+	device.playChirp("qpk93solit6k530de9");
+	device.playChirp("qpk93solit6k530de9");
+	Sleep(5000);
+	device.stopDevice();
+    return 0;
+}
+
